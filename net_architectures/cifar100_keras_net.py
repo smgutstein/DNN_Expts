930c9bb97ab953d5202d790936ff2d71810490d0
S. Gutstein <s.m.gutstein@gmail.com>
2018-12-25 17:07:52 -0500

    Activation layers labeled by function

--- net_architectures/cifar100_keras_net.py
+++ net_architectures/cifar100_keras_net.py
@@ -13,24 +13,24 @@ def build_architecture(input_shape,
     model.add(Convolution2D(32, (3, 3), padding='same',
               input_shape=input_shape))
 
-    model.add(Activation('relu'))
+    model.add(Activation('relu', name='relu_1'))
     model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
+    model.add(Activation('relu',name='relu_2') )
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Conv2D(64, (3, 3), padding='same'))
-    model.add(Activation('relu'))
+    model.add(Activation('relu', name='relu_3'))
     model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
+    model.add(Activation('relu', name='relu_4'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Flatten())
     model.add(Dense(512))
-    model.add(Activation('relu'))
+    model.add(Activation('relu', name='relu_5'))
     model.add(Dropout(0.5))
     model.add(Dense(nb_output_nodes))
-    model.add(Activation(output_activation))
+    model.add(Activation(output_activation, name=output_activation+'_out'))
 
     return model
