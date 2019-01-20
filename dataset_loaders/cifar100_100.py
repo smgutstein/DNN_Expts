f6c8e53e0f874ae683db9dd23672a26b2cb02980
S. Gutstein <s.m.gutstein@gmail.com>
2019-01-19 20:59:25 -0500

    Subset loaders

---
+++ dataset_loaders/cifar100_100.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+import os
+import sys
+file_dir = os.path.dirname(os.path.realpath(__file__))
+if file_dir not in sys.path:
+    sys.path.append(file_dir)
+from cifar import load_batch
+from keras.utils.data_utils import get_file
+from keras import backend as K
+import numpy as np
+from os.path import expanduser
+home = expanduser("~")
+
+
+def load_data(label_mode='fine'):
+    """Loads CIFAR100 dataset.
+
+    # Arguments
+        label_mode: one of "fine", "coarse".
+
+    # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+
+    # Raises
+        ValueError: in case of invalid `label_mode`.
+    """
+    if label_mode not in ['fine', 'coarse']:
+        raise ValueError('`label_mode` must be one of `"fine"`, `"coarse"`.')
+
+    path = os.path.join(home,'.keras/datasets/', 'cifar-100-python_100')
+
+    fpath = os.path.join(path, 'train')
+    x_train, y_train = load_batch(fpath, label_key=label_mode + '_labels')
+
+    fpath = os.path.join(path, 'test')
+    x_test, y_test = load_batch(fpath, label_key=label_mode + '_labels')
+
+    y_train = np.reshape(y_train, (len(y_train), 1))
+    y_test = np.reshape(y_test, (len(y_test), 1))
+
+    if K.image_data_format() == 'channels_last':
+        x_train = x_train.transpose(0, 2, 3, 1)
+        x_test = x_test.transpose(0, 2, 3, 1)
+
+    return (x_train, y_train), (x_test, y_test)
