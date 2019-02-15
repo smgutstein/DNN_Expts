39e55b7beec9e780bc4206049698441381e7de15
Steven Gutstein <s.m.gutstein@gmail.com>
2019-02-15 12:18:19 -0600

    Reducing Theano specific code

--- local_metric.py
+++ local_metric.py
@@ -14,41 +14,6 @@ def hot_bit(y_true, y_pred):
                           K.argmax(y_pred, axis=-1)),
                   K.floatx())
 
-
-def nearest_target(x, targets):
-    # Generate Cartesian Product of rows to compare
-    # i = T.ivector('i')
-    # j = T.ivector('j')
-
-    i_values = T.arange(x.shape[0]).repeat(targets.shape[0])  # outer rows/loop
-    j_values = T.tile(T.arange(targets.shape[0]), x.shape[0])  # inner rows/loop
-
-    # Get dists between each pair of rows in x & targets
-    dists, updates = theano_scan(lambda ii, jj, xx, yy: T.sum((xx[ii, :] - yy[jj, :])**2),
-                                 sequences=[i_values, j_values], non_sequences=[x, targets])
-    
-    # Get indices of rows in targets that are closest to each row of x
-    indices = T.flatten(T.argmin(dists.reshape((x.shape[0], targets.shape[0])), axis=-1))
-
-    # Recover each targets-row from the indices
-    nn, updates = theano_scan(lambda idx, targets: targets[idx, :],
-                              sequences=[indices],
-                              non_sequences=targets) 
-
-    return nn
-
-def ECOC_accuracy(y_encode):
-    def calc_acc(y_true, y_pred):
-        acc_list, updates = theano_scan(fn=lambda a, b: LK.allclose(a, b),
-                                        sequences=[y_true, y_pred])
-        return LK.true_div(acc_list.sum(), acc_list.size)
-    
-    def ECOC_fnc(y_true, y_pred):
-        return K.cast(K.mean(calc_acc(y_true,
-                      nearest_target(y_pred, y_encode))),
-                      K.floatx())
-    return ECOC_fnc
-
 def ECOC_fast_accuracy(y_encode):
     Y_Encoding = T.constant(y_encode, dtype=K.floatx())
 
