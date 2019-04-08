f0733f323a09f673baf189554575077b3c3864ec
smgutstein <s.m.gutstein@gmail.com>
2019-04-07 20:44:43 -0400

    Added Adam optimization

--- local_optimizer.py
+++ local_optimizer.py
@@ -4,3 +4,10 @@ def sgd(sgd_params):
     # Stochastic gradient descent
     # e.g. SGD(lr=0.05, decay=1e-6, momentum=0.9, nesterov=True)
     return optimizers.SGD(**sgd_params)
+
+
+def adam(adam_params):
+    # ADAM - ADAptive Moment estimation
+    # e.g. Adam(lr=0.001, beta_1=0.9, beta_2=0.999,
+    #             epsilon=None, decay=0., amsgrad=False)
+    return optimizers.Adam(**adam_params)
