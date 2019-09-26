e090c31da3adf5099b7b74f36b398839d7c30277
smgutstein <s.m.gutstein@gmail.com>
2019-09-26 18:07:41 -0400

    Initial draft of code to split CIFAR100 into source and target tasks

---
+++ make_cifar_src_trgt_tasks.py
@@ -0,0 +1,74 @@
+import argparse
+from collections import defaultdict
+import configparser
+import errno
+import json
+import numpy as np
+import pickle
+import os
+from os.path import expanduser
+import shutil
+
+
+
+def get_subtasks(subtasks, dataset, samps_per_class, num_classes):
+
+    print("Loading training set")
+    data_dict  = pickle.load(open(dataset,"rb"), encoding='latin1')
+
+    # Initialze info for subset_dict
+    subset_data = np.zeros((samps_per_class*num_classes, 3072))
+    subset_dict = dict()
+    subset_dict['fine_labels'] = []
+    subset_dict['coarse_labels'] = []
+    subset_dict['filenames'] = [] 
+    subset_dict['batch_label'] = "Subset training batch 1 of 1 - " + str(samps_per_class*num_classes)
+    subset_dict['batch_label'] += " samps per class"
+
+    for filename, batch, fine, coarse, data in zip(data_dict['filenames'], data_dict['batch_label'],
+                                                   data_dict['fine_labels'], data_dict['coarse_labels'],
+                                                   data_dict['data']):
+
+
+
+
+if __name__ == '__main__':
+
+    # Get desired samples per class
+    ap = argparse.ArgumentParser()
+    ap.add_argument("infile", type = str,
+                    help="file with input parameters")
+    args = ap.parse_args()
+
+    config = configparser.ConfigParser()
+    config.read(args.infile)
+    src_tasks = [x.strip() for x in config.get("Tasks","source").split(",")]
+    trgt_tasks = [x.strip() for x in config.get("Tasks","target").split(",")]
+
+    
+    # Get source dir for cifar 100 data
+    home = expanduser("~")
+    dataset_dir = '.keras/datasets/'
+    src_dir = 'cifar-100-python'
+    src_path = os.path.join(home, dataset_dir, src_dir)
+
+    print("Loading training set")
+    train  = pickle.load(open(os.path.join(src_path,"train"),'r'))
+    
+    print("Loading testing set")
+    test  = pickle.load(open(os.path.join(src_path,"test"),'r'))
+    
+
+    '''
+    # Make target dir and copy info from src dir
+    trgt_dir = 'cifar-100-python' + "_" + str(args.spc)
+    trgt_path = os.path.join(home, dataset_dir, trgt_dir)
+
+    # Note: shutil.copytree calls os.makedirs and will fail if trgt_path exists
+    shutil.copytree(src_path, trgt_path, symlinks=False, ignore=None)
+
+    # Save training subset
+    sd = get_subset(args.spc)
+    pickle.dump(sd, open(os.path.join(trgt_path, 'train'),'wb'))
+    print("Done")
+    '''
