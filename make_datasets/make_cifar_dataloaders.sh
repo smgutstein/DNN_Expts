b0b317f7bc1d21bc83e7ed9251452e007a988e1e
smgutstein <s.m.gutstein@gmail.com>
2020-01-17 14:06:43 -0500

    Files used to make datasets (only semi-tested)

---
+++ make_datasets/make_cifar_dataloaders.sh
@@ -0,0 +1,47 @@
+python generate_cifar100_trgt_subset_dataloaders.py 125_a
+python generate_cifar100_trgt_subset_dataloaders.py 125_b
+python generate_cifar100_trgt_subset_dataloaders.py 125_c
+python generate_cifar100_trgt_subset_dataloaders.py 125_d
+python generate_cifar100_trgt_subset_dataloaders.py 125_e
+
+python generate_cifar100_trgt_subset_dataloaders.py 175_a
+python generate_cifar100_trgt_subset_dataloaders.py 175_b
+python generate_cifar100_trgt_subset_dataloaders.py 175_c
+python generate_cifar100_trgt_subset_dataloaders.py 175_d
+python generate_cifar100_trgt_subset_dataloaders.py 175_e
+
+#python generate_cifar100_trgt_subset_dataloaders.py 25_b
+#python generate_cifar100_trgt_subset_dataloaders.py 25_c
+#python generate_cifar100_trgt_subset_dataloaders.py 25_d
+#python generate_cifar100_trgt_subset_dataloaders.py 25_e
+
+#python generate_cifar100_trgt_subset_dataloaders.py 50_b
+#python generate_cifar100_trgt_subset_dataloaders.py 50_c
+#python generate_cifar100_trgt_subset_dataloaders.py 50_d
+#python generate_cifar100_trgt_subset_dataloaders.py 50_e
+
+#python generate_cifar100_trgt_subset_dataloaders.py 75_b
+#python generate_cifar100_trgt_subset_dataloaders.py 75_c
+#python generate_cifar100_trgt_subset_dataloaders.py 75_d
+#python generate_cifar100_trgt_subset_dataloaders.py 75_e
+
+#python generate_cifar100_trgt_subset_dataloaders.py 100_b
+#python generate_cifar100_trgt_subset_dataloaders.py 100_c
+#python generate_cifar100_trgt_subset_dataloaders.py 100_d
+#python generate_cifar100_trgt_subset_dataloaders.py 100_e
+
+#python generate_cifar100_trgt_subset_dataloaders.py 150_b
+#python generate_cifar100_trgt_subset_dataloaders.py 150_c
+#python generate_cifar100_trgt_subset_dataloaders.py 150_d
+#python generate_cifar100_trgt_subset_dataloaders.py 150_e
+
+#python generate_cifar100_trgt_subset_dataloaders.py 200_b
+#python generate_cifar100_trgt_subset_dataloaders.py 200_c
+#python generate_cifar100_trgt_subset_dataloaders.py 200_d
+#python generate_cifar100_trgt_subset_dataloaders.py 200_e
+
+#python generate_cifar100_trgt_subset_dataloaders.py 250_b
+#python generate_cifar100_trgt_subset_dataloaders.py 250_c
+#python generate_cifar100_trgt_subset_dataloaders.py 250_d
+#python generate_cifar100_trgt_subset_dataloaders.py 250_e
+
