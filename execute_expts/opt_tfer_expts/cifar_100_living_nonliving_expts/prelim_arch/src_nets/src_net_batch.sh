a08bb6c571a1afe39e44b3dc84f2f93ee4074443
smgutstein <s.m.gutstein@gmail.com>
2020-03-07 22:01:10 -0500

    Run series of src net expts

---
+++ execute_expts/opt_tfer_expts/cifar_100_living_nonliving_expts/prelim_arch/src_nets/src_net_batch.sh
@@ -0,0 +1,7 @@
+#!  /bin/bash 
+
+python run_expt.py ./cfg_dir/expt_cfg/opt_tfer_expts/cifar_100_living_notliving_expts/prelim_arch/src_nets/src_net_0.cfg
+python run_expt.py ./cfg_dir/expt_cfg/opt_tfer_expts/cifar_100_living_notliving_expts/prelim_arch/src_nets/src_net_1.cfg
+python run_expt.py ./cfg_dir/expt_cfg/opt_tfer_expts/cifar_100_living_notliving_expts/prelim_arch/src_nets/src_net_2.cfg
+python run_expt.py ./cfg_dir/expt_cfg/opt_tfer_expts/cifar_100_living_notliving_expts/prelim_arch/src_nets/src_net_3.cfg
+python run_expt.py ./cfg_dir/expt_cfg/opt_tfer_expts/cifar_100_living_notliving_expts/prelim_arch/src_nets/src_net_4.cfg
