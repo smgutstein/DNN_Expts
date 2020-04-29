f67680f195f347b0b03fc2a5082a1af3d0d592c4
smgutstein <s.m.gutstein@gmail.com>
2020-04-29 01:34:43 -0400

    Adding some batch files

---
+++ execute_expts/opt_tfer_expts/cifar_100_ref_expts/vgg16/ref_nets/batch_vgg_abc.sh
@@ -0,0 +1,5 @@
+#!  /bin/bash 
+
+python run_expt.py ./cfg_dir/expt_cfg/opt_tfer_expts/cifar_100_reference_expts/vgg16/ref_nets/ref_net_a.cfg
+python run_expt.py ./cfg_dir/expt_cfg/opt_tfer_expts/cifar_100_reference_expts/vgg16/ref_nets/ref_net_b.cfg
+python run_expt.py ./cfg_dir/expt_cfg/opt_tfer_expts/cifar_100_reference_expts/vgg16/ref_nets/ref_net_c.cfg
---
+++ execute_expts/opt_tfer_expts/cifar_100_ref_expts/vgg16/ref_nets/batch_vgg_de.sh
@@ -0,0 +1,4 @@
+#!  /bin/bash 
+
+python run_expt.py ./cfg_dir/expt_cfg/opt_tfer_expts/cifar_100_reference_expts/vgg16/ref_nets/ref_net_d.cfg
+python run_expt.py ./cfg_dir/expt_cfg/opt_tfer_expts/cifar_100_reference_expts/vgg16/ref_nets/ref_net_e.cfg
