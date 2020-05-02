7b0ff63e91af62b245e89bd1dc31927e2b06e598
smgutstein <s.m.gutstein@gmail.com>
2020-05-01 22:46:59 -0400

    Fixing vgg expts

--- lr_sched_fncs/vgg16_sched.py
+++ lr_sched_fncs/vgg16_sched.py
@@ -1,7 +1,9 @@
-def lr_sched_func(lr, kwargs):
+def lr_sched_func(lr, lr_orig, kwargs):
 
     epoch = kwargs["epoch"] 
     lr_scale = kwargs['lr_scale']
-    lr = lr  * (0.5 ** (epoch // lr_scale))
+
+    lr = lr_orig  * (0.5 ** (epoch // lr_scale))
+    print("lr = ", lr, " * ", "0.5 ** (", epoch, "//", lr_scale,")")
 
     return lr
