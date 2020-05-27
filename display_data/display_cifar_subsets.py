dc94414708c4de1583cbf19f92ab4c4f2ad1fd8d
smgutstein <s.m.gutstein@gmail.com>
2020-05-27 16:30:15 -0400

    Adjusting for new directory structure

--- display_data/display_cifar_subsets.py
+++ display_data/display_cifar_subsets.py
@@ -7,11 +7,11 @@ import pickle
 import sys
 
 file_dir = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), 'dataset_loaders')
+    os.path.dirname(os.path.realpath(__file__)), '../dataset_loaders')
 if file_dir not in sys.path:
     sys.path.append(file_dir)
 
-dict_path = 'dataset_info/cifar100_dicts_all.pkl'
+dict_path = '../dataset_info/cifar100_dicts_all.pkl'
 with open(dict_path,'rb') as f:
     label_dict = pickle.load(f, encoding='latin1')
     
@@ -50,4 +50,5 @@ if __name__ == '__main__':
     data_display = Data_Display(X_train, y_train, label_dict)
     data_display.start_display()
 
-# e.g. python display_cifar_subsets.py cifar100_src_living_vs_notliving
+# e.g. python display_cifar_subsets.py cifar100_src_v0_living_vs_notliving
+#      python display_cifar_subsets.py cifar100_trgt_v0_living_vs_notliving
