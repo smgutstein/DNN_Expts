f42aa573c0de28211c2be1f9cdebaad3053bb48d
Steven Gutstein <s.m.gutstein@gmail.com>
2020-07-09 23:43:39 -0400

    Separating static strings for dataset loaders from module

--- make_datasets/make_cifar_trgt_dataloaders.py
+++ make_datasets/make_cifar_trgt_dataloaders.py
@@ -3,69 +3,9 @@ import configparser
 import itertools
 import os
 
-import_str = '''
-from __future__ import absolute_import
-import os
-import sys
-file_dir = os.path.dirname(os.path.realpath(__file__))
-if file_dir not in sys.path:
-    sys.path.append(file_dir)
-from cifar import load_batch
-from keras.utils.data_utils import get_file
-from keras import backend as K
-import numpy as np
-from os.path import expanduser
-home = expanduser("~")
+from trgt_task_dataloader_strings import import_str, header_str
+from trgt_task_dataloader_strings import doc_body_str, path_str, body_str
 
-'''
-
-header_str = '''
-def load_data(label_mode='fine'):
-'''
-
-doc_str_body = '''
-    
-    
-    # Arguments
-        label_mode: one of \"fine\", \"coarse\".
-
-    # Returns
-        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
-
-    # Raises
-        ValueError: in case of invalid `label_mode`.
-
-'''
-
-path_str = '''
-    path = os.path.join(home'''
-
-body_str = '''
-    if label_mode not in [\'fine\', \'coarse\']:
-        raise ValueError(\'`label_mode` must be one of `\"fine\"`, `\"coarse\"`.')
-
-    fpath = os.path.join(path, \'train\')
-    x_train, y_train = load_batch(fpath, label_key=label_mode + \'_labels\')
-
-    fpath = os.path.join(path, 'test')
-    x_test, y_test = load_batch(fpath, label_key=label_mode + \'_labels\')
-
-    y_train = np.reshape(y_train, (len(y_train), 1))
-    y_test = np.reshape(y_test, (len(y_test), 1))
-
-    # Rescale raw data
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-
-    x_train /= 255.
-    x_test /= 255.
-
-    if K.image_data_format() == \'channels_last\':
-        x_train = x_train.transpose(0, 2, 3, 1)
-        x_test = x_test.transpose(0, 2, 3, 1)
-
-    return (x_train, y_train), (x_test, y_test)
-'''
 
 if __name__ == '__main__':
     # Get desired samples per class
@@ -90,7 +30,7 @@ if __name__ == '__main__':
     config.read(config_file)
 
     note = "Loads data for " + config['Notes']['note']
-    doc_str = '    \"\"\"\n' + '    ' + note + doc_str_body + '    \"\"\"\n'
+    doc_body_str = '    \"\"\"\n' + '    ' + note + doc_body_str + '    \"\"\"\n'
 
     data_root_dir = config['StorageDirectory']['data_root_dir']
     data_dir = config['StorageDirectory']['data_dir']
@@ -110,7 +50,7 @@ if __name__ == '__main__':
         data_path = "_".join([path_str, str(spc), suffix + "'"])
         data_path += ")"
 
-        out_str = import_str + header_str + doc_str + data_path + body_str
+        out_str = import_str + header_str + doc_body_str + data_path + body_str
         out_path = '../dataset_loaders/'
         out_file = '_'.join([subset_root_dir,subset_dir,spc,suffix]) + '.py'
         
