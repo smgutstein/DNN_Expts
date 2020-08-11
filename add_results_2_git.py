19aaf4a0322630339251caf203b5ecd80477e504
Steven Gutstein <s.m.gutstein@gmail.com>
2020-08-10 22:47:10 -0400

    Code to allow sharing of result files with git

---
+++ add_results_2_git.py
@@ -0,0 +1,40 @@
+from git import Repo
+import os
+
+
+def find_files(curr_dir, found_files=None):
+    if found_files is None:
+        found_files = []
+    curr_sub_dirs = [x for x in os.listdir(curr_dir)
+                     if os.path.isdir(os.path.join(curr_dir, x))]
+    
+    curr_found_files = [os.path.join(curr_dir,x)
+                        for x in os.listdir(curr_dir)
+                        if 'results.txt' == x or 'Expt_output.log' == x]
+
+    found_files += curr_found_files
+
+    for curr_sub_dir in curr_sub_dirs:
+        found_files = find_files(os.path.join(curr_dir, curr_sub_dir),
+                                 found_files)
+
+    return found_files
+    
+    
+def git_add(add_files):
+    repo = Repo('.')
+    for curr_file in add_files:
+        repo.git.add('-f', curr_file)
+
+def git_commit():
+    repo = Repo('.')
+    repo.git.commit('-m','adding results to tfer to/from BIERS cluster')
+
+result_root_list = ['./results/opt_tfer_expts/cifar_100_living_notliving_expts/wide_resnet_28_10_arch/src_nets/workshop_expts',
+                    './results/opt_tfer_expts/cifar_100_living_notliving_2_expts/wide_resnet_28_10_arch/tfer_nets']
+
+for curr_result_root in result_root_list:
+    found_files = find_files(curr_result_root)    
+    git_add(found_files)
+
+git_commit()
